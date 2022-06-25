5dd0724457553d9c5af90a6cbefe97e4deb9ea61
Michael Nelson <kofinelson307@gmail.com>
2022-06-24 22:38:47 -0400

    using the input function and for loops

--- password_generator.py
+++ password_generator.py
@@ -2,3 +2,22 @@
 import random
 
 print(""" --------- Random Password Generator --------- """)
+print("\n")
+
+# listing the characters for the password
+pass_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_+=[{]}|\?/.><,"
+
+# length of password. The user would have to choose a string of 8 characters or more
+len_of_password = int(
+    input("What would you like to be the length of your password? "))
+
+# number of passwords the user would like to generate
+num_of_passwords = int(input("How many passwords would like to generate? "))
+
+print('\nhere are your passwords: ')
+
+for pswd in range(num_of_passwords):
+    passwords = ""
+    for c in range(len_of_password):
+        passwords += random.choice(pass_characters)
+    print(passwords)
